f85f747c55d7f6154eaacaeb9a48f64593bbaecb
Mark-Stessl <124023885+Mark-Stessl@users.noreply.github.com>
2024-12-10 22:45:43 +0100

    Sql file

---
+++ gamehub.sql
@@ -0,0 +1,99 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 16.0
+-- Dumped by pg_dump version 16.0
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: games; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.games (
+    id integer NOT NULL,
+    title character varying(255) NOT NULL,
+    genre character varying(100),
+    platform character varying(100),
+    rating numeric,
+    release_date date,
+    img text
+);
+
+
+ALTER TABLE public.games OWNER TO postgres;
+
+--
+-- Name: games_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.games_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.games_id_seq OWNER TO postgres;
+
+--
+-- Name: games_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.games_id_seq OWNED BY public.games.id;
+
+
+--
+-- Name: games id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.games ALTER COLUMN id SET DEFAULT nextval('public.games_id_seq'::regclass);
+
+
+--
+-- Data for Name: games; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.games (id, title, genre, platform, rating, release_date, img) FROM stdin;
+1	The Legend of Zelda: Breath of the Wild	Adventure	Nintendo Switch	10	2017-03-03	images/botw.jpg
+2	Elden Ring	RPG	PlayStation 5	9.5	2022-02-25	images/eldenring.jpg
+3	Animal Crossing: New Horizons	Simulation	Nintendo Switch	9	2020-03-20	images/acnh.png
+4	God of War Ragnarök	Action	PlayStation 5	10	2022-11-09	images/godofwar.jpg
+\.
+
+
+--
+-- Name: games_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.games_id_seq', 4, true);
+
+
+--
+-- Name: games games_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.games
+    ADD CONSTRAINT games_pkey PRIMARY KEY (id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
