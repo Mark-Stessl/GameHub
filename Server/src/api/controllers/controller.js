97a4076b9863f2fe2081d0e9dc701e2223c4c4e2
Mark-Stessl <124023885+Mark-Stessl@users.noreply.github.com>
2025-02-13 08:13:54 +0100

    Delete Server directory

--- Server/src/api/controllers/controller.js
+++
@@ -1,6 +0,0 @@
-import asyncHandler from 'express-async-handler';
-import { getAllGames } from '../models/model.js';
-
-export const getGames = asyncHandler(async (req, res) => {
-  res.status(200).json(await getAllGames());
-});
--- Server/src/api/routes/route.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import asyncHandler from 'express-async-handler';
-import { getGames } from '../controllers/controller.js';
-const router = express.Router();
-
-router.get('/', asyncHandler(getGames));
-
-export default router;
